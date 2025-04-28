756d2588795ca39f513d15065c27ee6d274707bc
Mark A J Checkley <mark@checkleynet.co.uk>
2025-04-28 15:28:34 +0100

    Spotted a bug

--- resources/views/coffee_sales.blade.php
+++ resources/views/coffee_sales.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200 h-100">
                     <form name="coffee-sale" id="coffee-sale" method="post" action="/sales">
                         @csrf
                         <div class="grid grid-cols-5 gap-5">
@@ -33,7 +33,7 @@
                                 <div class="py-2">
                                     <label id="selling-price">£0.00</label>
                                 </div>
-                            </div class="w-[1/5]">
+                            </div>
                             <button id="record-sale" class="self-end border-none h-10 bg-indigo-500 text-white font-bold py-1 px-2 rounded" type="submit">{{ __('sales.record_sale') }}</button>
                         </div>
                     </form>
